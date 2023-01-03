701afec3d8d364405a42c7bb126a83afef240373
ydaniju <danijuyusuf@outlook.com>
2023-01-03 20:11:50 +0100

    room service

---
+++ booting-web/room-web-app/src/main/java/com/ydaniju/lil/sbet/landon/roomwebapp/controllers/RoomRestController.java
@@ -0,0 +1,24 @@
+package com.ydaniju.lil.sbet.landon.roomwebapp.controllers;
+
+import com.ydaniju.lil.sbet.landon.roomwebapp.models.Room;
+import com.ydaniju.lil.sbet.landon.roomwebapp.services.RoomService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/rooms")
+public class RoomRestController {
+    private final RoomService roomService;
+
+    public RoomRestController(RoomService roomService) {
+        this.roomService = roomService;
+    }
+
+    @GetMapping
+    public List<Room> getAllRooms() {
+        return RoomService.getAllRooms();
+    }
+}
--- booting-web/room-web-app/src/main/java/com/ydaniju/lil/sbet/landon/roomwebapp/services/RoomService.java
+++ booting-web/room-web-app/src/main/java/com/ydaniju/lil/sbet/landon/roomwebapp/services/RoomService.java
@@ -1,11 +1,13 @@
 package com.ydaniju.lil.sbet.landon.roomwebapp.services;
 
 import com.ydaniju.lil.sbet.landon.roomwebapp.models.Room;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Component
 public class RoomService {
     private static final List<Room> rooms = new ArrayList<>();
 
