19b8c5f8d9ee7882c3c6a8e86741b5d7a80ab101
ydaniju <danijuyusuf@outlook.com>
2023-01-03 16:58:44 +0100

    create woom page

---
+++ booting-web/room-web-app/src/main/java/com/ydaniju/lil/sbet/landon/roomwebapp/controllers/RoomController.java
@@ -0,0 +1,30 @@
+package com.ydaniju.lil.sbet.landon.roomwebapp.controllers;
+
+import com.ydaniju.lil.sbet.landon.roomwebapp.models.Room;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/rooms")
+public class RoomController {
+    private static final List<Room> rooms = new ArrayList<>();
+
+    static {
+        for (int i = 0; i < 10; i++) {
+            rooms.add(new Room(i, "Room " + i, "R" + i, "Q"));
+        }
+    }
+
+    @GetMapping
+    public String getAllRooms(Model model) {
+        model.addAttribute("rooms", rooms);
+
+        return "rooms";
+    }
+}
