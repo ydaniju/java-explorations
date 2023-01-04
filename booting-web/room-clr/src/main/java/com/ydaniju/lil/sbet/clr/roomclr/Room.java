2377aff81ae97c7c82bb58cbf3ba3da4bf19d686
ydaniju <danijuyusuf@outlook.com>
2023-01-04 12:15:24 +0100

    room clr app

---
+++ booting-web/room-clr/src/main/java/com/ydaniju/lil/sbet/clr/roomclr/Room.java
@@ -0,0 +1,49 @@
+package com.ydaniju.lil.sbet.clr.roomclr;
+
+import java.util.UUID;
+
+public class Room {
+    private UUID id;
+    private String name;
+    private String number;
+    private String info;
+
+    public Room(UUID id, String name, String number, String info) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.info = info;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
---
+++ booting-web/room-clr/src/main/java/com/ydaniju/lil/sbet/clr/roomclr/RoomClrApplication.java
@@ -0,0 +1,13 @@
+package com.ydaniju.lil.sbet.clr.roomclr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomClrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoomClrApplication.class, args);
+    }
+
+}
